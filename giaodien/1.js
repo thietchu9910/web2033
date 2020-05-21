9267b2b072a02cf24d42770c8ce50118d6aa2a36
thietchu9910 <61450288+thietchu9910@users.noreply.github.com>
2020-05-21 22:49:59 +0700

    html

---
+++ giaodien/1.js
@@ -0,0 +1,28 @@
+﻿ $(function(){
+ 	$(window).resize(function(event) {
+ 		if($(window).width() < 576){ 			 
+ 			$('.menutop ul li ul').slideUp();
+ 		}
+ 	});
+ 	$('.menutop .navbar-light .navbar-nav > li ') .bind('touchstart',function(){		 
+		$(this).children('ul').slideToggle();
+ 	})
+ 
+ 	// jquery cho phan hieu ung 
+ 	$('ul.tdtab li b').click(function(){
+ 		$('ul.tdtab li b').removeClass('active');
+ 		$(this).addClass('active');
+
+ 		chiso = $(this).parent().index() + 1;
+ 		
+
+ 		$('ul.ndtab li .divsanpham').removeClass('xuathien');
+ 		$('ul.ndtab li:nth-child(' + chiso + ") .divsanpham ").addClass('xuathien');
+
+
+ 		 
+
+ 	})
+
+})  
+ 
